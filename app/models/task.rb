a17ada4e8c68fbf89c6da55251c2bafb001747b5
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-17 12:21:25 -0400

    work through task_manager

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 # app/models/task.rb
 
 class Task < ApplicationRecord
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
+  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  
+  patch '/tasks/:id', to: 'tasks#update'
+
   post '/tasks', to: 'tasks#create'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 end
